cb427db7d1e64b48003ef6aecab4c20f5c2d1ae7
Gabriel Rossetto <gabrielrossetto@192.168.1.227>
2025-03-24 13:38:31 +0000

    adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import solutions2 from "./assets/digitalmarketing.jpg";
 import solutions3 from "./assets/responsive.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { FaInstagram, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +22,7 @@ function Header() {
 
       {/* Mobile Menu Button */}
       <button
-        className="md:hidden text-green-400 focus:outline-none"
+        className="md:hidden text-green-400 focus:outline-none px-6" // Add px-6 to the button for padding
         onClick={() => setIsOpen(!isOpen)}
         aria-label="Toggle Menu"
       >
@@ -33,12 +34,12 @@ function Header() {
         className={`${
           isOpen ? "flex border-b border-green-400" : "hidden"
         } md:flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6 
-        fixed md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-6 md:p-0 z-40`}
+    fixed md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-6 md:p-0 z-40 px-6 md:px-24`} // Add px-6 and md:px-24 for padding
       >
-        <a href="#about" className="text-gray-400 hover:text-green-400">
+        <a href="#about" className="text-white hover:text-green-400">
           Soluções
         </a>
-        <a href="#experience" className="text-gray-400 hover:text-green-400">
+        <a href="#experience" className="text-white hover:text-green-400">
           Quem Somos
         </a>
         <button className="border border-green-400 px-4 py-2 text-green-400 hover:bg-green-400 hover:text-gray-900">
@@ -83,6 +84,87 @@ function Hero({ scrollToSection }) {
     </section>
   );
 }
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-12 px-4 md:px-16 font-mono">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between ">
+        {/* Logo and Description */}
+        <div className="w-full md:w-2/8 mb-12">
+          <img className="w-40 h-8" src={gr} alt="GR Web Solutions" />
+          <p className="mt-4 text-white text-sm">
+            Crie experiências digitais impactantes e impulsione o seu negócio
+            com nossos serviços personalizados.
+          </p>
+          <div className="mt-6 flex items-center">
+            <input
+              type="email"
+              placeholder="Digite seu email aqui"
+              className="py-3 px-4 border placeholder-white border-green-400 rounded-l-lg w-full h-10"
+            />
+            <button className="bg-green-400 text-gray-900 text-sm px-4 h-full flex items-center justify-center rounded-r-lg">
+              Vamos conversar
+            </button>
+          </div>
+        </div>
+
+        {/* Company Links */}
+        <div className="w-full md:w-2/8 mb-12">
+          <h3 className="text-lg font-semibold text-green-400">Empresa</h3>
+          <ul className="mt-4 space-y-2">
+            <li>
+              <a href="#about" className="hover:text-green-400">
+                Soluções
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="hover:text-green-400">
+                Quem Somos
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="hover:text-green-400">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Social Media */}
+        <div className="w-full md:w-2/8 mb-12">
+          <h3 className="text-lg font-semibold text-green-400">
+            Redes sociais
+          </h3>
+          <ul className="mt-4 space-y-2">
+            <li className="flex items-center gap-3">
+              <FaWhatsapp className="text-green-400" />
+              <a href="#" className="hover:text-green-400">
+                WhatsApp
+              </a>
+            </li>
+            <li className="flex items-center gap-3">
+              <FaInstagram className="text-green-400" />
+              <a href="#" className="hover:text-green-400">
+                Instagram
+              </a>
+            </li>
+            <li className="flex items-center gap-3">
+              <FaLinkedinIn className="text-green-400" />
+              <a href="#" className="hover:text-green-400">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="text-center text-gray-500 text-sm">
+        2025 © GR Web Solutions. Criado e desenvolvido com carinho (e React).
+      </div>
+    </footer>
+  );
+};
 
 function App() {
   useEffect(() => {
@@ -129,7 +211,7 @@ function App() {
       <Hero scrollToSection={scrollToSection} />
       <section id="solucoes" className="px-6 md:px-24 py-24">
         <h2 className="mb-24 text-center font-mono text-6xl font-bold text-gray-900 tracking-tight">
-          <span className="text-green-400">Nossas Soluções_</span>
+          Nossas Soluções_
         </h2>
 
         <div className="flex flex-col gap-16">
@@ -159,6 +241,7 @@ function App() {
           ))}
         </div>
       </section>
+      <Footer />
     </div>
   );
 }
